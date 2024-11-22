0d61865b27441e67a1baab8dcb05389ae8df11b2
JHolland <20102340@mail.wit.ie>
2024-11-22 17:21:56 +0000

    Actor List added

--- movies/src/components/actorList/index.js
+++ movies/src/components/actorList/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Movie from "../movieCard/";
+import Actor from "../actorCard/";
 import Grid from "@mui/material/Grid2";
 
-const MovieList = (props) => {
-  let movieCards = (props.movies || []).map((m) => (
+const ActorList = (props) => {
+  let actorCards = (props.actors || []).map((m) => (
     <Grid key={m.id} size={{xs: 12, sm: 6, md: 4, lg: 3, xl: 2}} sx={{padding: "20px"}}>
     <Movie key={m.id} movie={m} selectFavorite={props.selectFavorite} />
     </Grid>
@@ -11,9 +11,9 @@ const MovieList = (props) => {
 
   return (
     <Grid container spacing={2}>
-      {movieCards.length > 0 ? movieCards : <p>No movies available.</p>}
+      {actorCards.length > 0 ? actorCards : <p>No actors available.</p>}
     </Grid>
   );
 };
 
-export default MovieList;
+export default ActorList;
