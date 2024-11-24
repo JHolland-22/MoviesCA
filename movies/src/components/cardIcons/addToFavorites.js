b765b758b1c96fb9963bfc8e8c6af50e131ac5d4
JHolland <20102340@mail.wit.ie>
2024-11-24 15:42:04 +0000

    added in comments

--- movies/src/components/cardIcons/addToFavorites.js
+++ movies/src/components/cardIcons/addToFavorites.js
@@ -4,16 +4,18 @@ import IconButton from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const AddToFavoritesIcon = ({ movie }) => {
+  // Access the MoviesContext to manage movie favorites
   const context = useContext(MoviesContext);
 
+  // Function to handle adding the movie to favorites
   const handleAddToFavorites = (e) => {
-    e.preventDefault();
-    context.addToFavorites(movie);
+    e.preventDefault(); // Prevent default behavior (e.g., page refresh on form submission)
+    context.addToFavorites(movie); // Add the movie to favorites using the context function
   };
 
   return (
     <IconButton aria-label="add to favorites" onClick={handleAddToFavorites}>
-      <FavoriteIcon color="primary" fontSize="large" />
+      <FavoriteIcon color="primary" fontSize="large" /> {/* Favorite icon for the button */}
     </IconButton>
   );
 };
